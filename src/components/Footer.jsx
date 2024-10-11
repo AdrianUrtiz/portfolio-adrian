4dc0ce724f4c5dcfeacc8e5d3c0d3c1e30e14726
AdrianUrtiz <116136643+AdrianUrtiz@users.noreply.github.com>
2024-10-11 16:51:10 -0600

    Centrando el teexto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className='flex justify-center items-center h-20 text-neutral-200 dark:bg-neutral-200 dark:text-neutral-800'>
-      <p className='text-sm'>
+      <p className='text-sm text-center'>
         &copy; {new Date().getFullYear()} Adrian Urtiz. Casi todos los derechos
         reservados.
       </p>

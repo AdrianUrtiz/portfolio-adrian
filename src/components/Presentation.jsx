0f1e8694316a386aa6ad96de176196823f7f053e
AdrianUrtiz <116136643+AdrianUrtiz@users.noreply.github.com>
2024-10-15 16:00:37 -0600

    Mejoras en el componente social item

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -111,13 +111,10 @@ function Presentation() {
           <Mail className='size-3 lg:size-4' />
           Email
         </SocialItem>
-        <a
-          className='rounded-xl border border-white/10 flex justify-center items-center gap-x-2 py-1 px-2 lg:py-2 lg:px-4 text-sm md:text-base bg-white/5 hover:bg-white/10 hover:scale-105 transition-transform duration-300 ease-in-out'
-          href='/CV Adrian Eduardo Urtiz Parra.pdf'
-          download>
+        <SocialItem href='/CV Adrian Eduardo Urtiz Parra.pdf' download={true}>
           <Download className='size-3 lg:size-4' />
           {translations.download}
-        </a>
+        </SocialItem>
       </motion.nav>
     </>
   )
--- src/components/SocialItem.jsx
+++ src/components/SocialItem.jsx
@@ -1,8 +1,9 @@
-const SocialItem = ({ children, href }) => {
+const SocialItem = ({ children, href, download = false }) => {
   return (
     <a
       className='rounded-xl border border-white/10 flex justify-center items-center gap-x-2 py-1 px-2 lg:py-2 lg:px-4 text-sm md:text-base bg-white/5 hover:bg-white/10 hover:scale-105 transition-transform duration-300 ease-in-out'
       href={href}
+      download={download}
       target='_blank'
       rel='noopener noreferrer'>
       {children}
